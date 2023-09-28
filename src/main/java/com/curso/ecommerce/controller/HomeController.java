08d2d882f191f617e5c4a41901c81cc8a70cf335
joseluisbalmaseda2023 <joseluisbalmaseda@gmail.com>
2023-09-29 00:13:02 +0200

    Redirccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
@@ -19,9 +20,8 @@ import com.curso.ecommerce.service.ProductoService;
 public class HomeController {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	
+
 	private Producto producto;
-	
 
 	@Autowired
 	private ProductoService productoService;
@@ -37,10 +37,20 @@ public class HomeController {
 	@GetMapping("productohome/{id}")
 	public String productoHome(Model model, @PathVariable Integer id) {
 		LOGGER.info("Id producto enviado como parametro {}: " + id);
-		Optional <Producto> productoOptoinal = productoService.findById(id);
+		Optional<Producto> productoOptoinal = productoService.findById(id);
 		producto = productoOptoinal.get();
 		model.addAttribute("producto", producto);
 		return "usuario/productohome";
+	}
+
+	@PostMapping("/cart")
+	public String addCart() {
+//		LOGGER.info("Id producto enviado al carrito como parametro {}: " + id);
+//		Optional<Producto> productoOptoinal = productoService.findById(id);
+//		producto = productoOptoinal.get();
+//		model.addAttribute("producto", producto);
+		
+		return "usuario/carrito";
 	}
 
 }
